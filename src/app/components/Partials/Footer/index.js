475f5d4c59b42b4babaa401ed1c7c837e9373d95
Ha Lan D <dnhalan0101@gmail.com>
2024-09-12 04:52:19 +0200

    add template profile

--- src/app/components/Partials/Footer/index.js
+++ src/app/components/Partials/Footer/index.js
@@ -2,23 +2,24 @@ import Link from "next/link";
 const Footer = () => {
 
   return (
-    <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="/" className="hover:underline">Wizard Planner</a>. All Rights Reserved.
-    </span>
-    <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-        <li>
-          <Link href="/" className="hover:underline me-4 md:me-6">About</Link>
-        </li>
-        <li>
-          <Link href="/" className="hover:underline me-4 md:me-6">Privacy Policy</Link>
-        </li>
-        <li>
-          <Link href="/" className="hover:underline me-4 md:me-6">Licensing</Link>
-        </li>
-        <li>
-          <Link href="/" className="hover:underline me-4 md:me-6">Contact</Link>
-        </li>
-    </ul>
+    <div className="bg-gray-800">
+      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between ">
+        <span className="text-xl text-white sm:text-center">© 2024 <a href="/" className="hover:underline">Wizard Planner</a>. All Rights Reserved.</span>
+        <ul className="flex flex-wrap items-center mt-3 text-xl font-medium text-white sm:mt-0">
+            <li>
+              <Link href="/" className="hover:underline me-4 md:me-6">About</Link>
+            </li>
+            <li>
+              <Link href="/" className="hover:underline me-4 md:me-6">Privacy Policy</Link>
+            </li>
+            <li>
+              <Link href="/" className="hover:underline me-4 md:me-6">Licensing</Link>
+            </li>
+            <li>
+              <Link href="/" className="hover:underline me-4 md:me-6">Contact</Link>
+            </li>
+        </ul>
+      </div>
     </div>
   );
 };
--- src/app/components/Partials/Header/index.js
+++ src/app/components/Partials/Header/index.js
@@ -44,7 +44,7 @@ const ChicHeader = ({ isLoggedIn }) => {
   return (
     <header className={`z-10 px-4 lg:px-6 py-2.5 w-full transition-all duration-300 ${
       isScrolled
-        ? 'fixed top-0 left-0 bg-black bg-opacity-50'
+        ? 'fixed top-0 left-0 bg-gray-800 bg-opacity-50'
         : 'absolute top-0 left-0 bg-transparent'
     }`}>
       <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
@@ -57,13 +57,13 @@ const ChicHeader = ({ isLoggedIn }) => {
               <>
                 <Link
                   href="/account/profile"
-                  className="text-white border border-white hover:bg-white hover:text-black focus:ring-4 focus:ring-white font-medium rounded-lg text-xl px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:text-white dark:hover:bg-white dark:hover:text-black dark:focus:ring-white focus:outline-none"
+                  className="text-white border border-white hover:bg-white hover:text-gray-900 focus:ring-4 focus:ring-white font-medium rounded-lg text-xl px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:text-white dark:hover:bg-white dark:hover:text-gray-900 dark:focus:ring-white focus:outline-none"
                 >
                   Profile
                 </Link>
                 <button
                   onClick={handleLogout}
-                  className="text-white border border-white hover:bg-white hover:text-black focus:ring-4 focus:ring-white font-medium rounded-lg text-xl px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:text-white dark:hover:bg-white dark:hover:text-black dark:focus:ring-white focus:outline-none"
+                  className="text-white border border-white hover:bg-white hover:text-gray-900 focus:ring-4 focus:ring-white font-medium rounded-lg text-xl px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:text-white dark:hover:bg-white dark:hover:text-gray-900 dark:focus:ring-white focus:outline-none"
                 >
                   Logout
                 </button>
@@ -71,12 +71,12 @@ const ChicHeader = ({ isLoggedIn }) => {
             ) : (
               <Link
                 href="/auth/login"
-                className="text-white border border-white text-xl hover:bg-white hover:text-black focus:ring-4 focus:ring-white font-medium rounded-lg px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:text-white dark:hover:bg-white dark:hover:text-black dark:focus:ring-white focus:outline-none"
+                className="text-white border border-white text-xl hover:bg-white hover:text-gray-900 focus:ring-4 focus:ring-white font-medium rounded-lg px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:text-white dark:hover:bg-white dark:hover:text-gray-900 dark:focus:ring-white focus:outline-none"
               >
                 Sign In
               </Link>
             )}
-          <button onClick={toggleMenu} type="button" className="inline-flex items-center p-2 ml-1 text-xl text-white border border-white rounded-lg lg:hidden hover:bg-white hover:text-black focus:outline-none focus:ring-2 focus:ring-white dark:text-white dark:hover:bg-white dark:hover:text-black dark:focus:ring-white" aria-controls="mobile-menu-2" aria-expanded={isMenuOpen}>
+          <button onClick={toggleMenu} type="button" className="inline-flex items-center p-2 ml-1 text-xl text-white border border-white rounded-lg lg:hidden hover:bg-white hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-white dark:text-white dark:hover:bg-white dark:hover:text-gray-900 dark:focus:ring-white" aria-controls="mobile-menu-2" aria-expanded={isMenuOpen}>
             <span className="sr-only">Open main menu</span>
             <svg className={`w-6 h-6 ${isMenuOpen ? 'hidden' : 'block'}`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
             <svg className={`w-6 h-6 ${isMenuOpen ? 'block' : 'hidden'}`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
@@ -87,14 +87,14 @@ const ChicHeader = ({ isLoggedIn }) => {
             <Link
               href="/plan-trip"
               onClick={() => handleLinkClick('/plan-trip')}
-              className={`block py-2 pr-4 pl-3 border-b border-white hover:bg-white hover:text-black lg:hover:bg-transparent lg:border-0 lg:p-0 dark:text-white dark:hover:bg-white dark:hover:text-black lg:dark:hover:bg-transparent text-xl ${activeLink === '/plan-trip' ? 'font-bold' : 'text-white'}`}
+              className={`block py-2 pr-4 pl-3 border-b border-white hover:bg-white hover:text-gray-900 lg:hover:bg-transparent lg:border-0 lg:p-0 dark:text-white dark:hover:bg-white dark:hover:text-gray-900 lg:dark:hover:bg-transparent text-xl ${activeLink === '/plan-trip' ? 'font-bold' : 'text-white'}`}
             >
               Plan Your Trip
             </Link>
             <Link
               href="/subscription"
               onClick={() => handleLinkClick('/subscription')}
-              className={`block py-2 pr-4 pl-3 border-b border-white hover:bg-white hover:text-black lg:hover:bg-transparent lg:border-0 lg:p-0 dark:text-white dark:hover:bg-white dark:hover:text-black lg:dark:hover:bg-transparent text-xl ${activeLink === '/subscription' ? 'font-bold' : 'text-white'}`}
+              className={`block py-2 pr-4 pl-3 border-b border-white hover:bg-white hover:text-gray-900 lg:hover:bg-transparent lg:border-0 lg:p-0 dark:text-white dark:hover:bg-white dark:hover:text-gray-900 lg:dark:hover:bg-transparent text-xl ${activeLink === '/subscription' ? 'font-bold' : 'text-white'}`}
             >
               Price
             </Link>
