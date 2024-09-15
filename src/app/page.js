91959afd6c6df80c51d6c1cd83ccdf1e14e231c5
Ha Lan D <dnhalan0101@gmail.com>
2024-09-15 23:15:13 +0200

    add photo

--- src/app/page.js
+++ src/app/page.js
@@ -157,46 +157,46 @@ export default function Home() {
           <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
               <div className="grid gap-4">
                   <div>
-                      <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" alt=""></img>
+                      <img className="h-auto max-w-full rounded-lg" src="/homepage/memories/img1.jpg" alt=""></img>
                   </div>
                   <div>
-                      <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" alt=""></img>
+                      <img className="h-auto max-w-full rounded-lg" src="/homepage/memories/img5.jpg" alt=""></img>
                   </div>
                   <div>
-                      <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" alt=""></img>
-                  </div>
-              </div>
-              <div className="grid gap-4">
-                  <div>
-                      <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt=""></img>
-                  </div>
-                  <div>
-                      <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" alt=""></img>
-                  </div>
-                  <div>
-                      <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg" alt=""></img>
+                      <img className="h-auto max-w-full rounded-lg" src="/homepage/memories/img9.jpeg" alt=""></img>
                   </div>
               </div>
               <div className="grid gap-4">
                   <div>
-                      <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg" alt=""></img>
+                      <img className="h-auto max-w-full rounded-lg" src="/homepage/memories/img4.jpg" alt=""></img>
                   </div>
                   <div>
-                      <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" alt=""></img>
+                      <img className="h-auto max-w-full rounded-lg" src="/homepage/memories/img2.jpg" alt=""></img>
                   </div>
                   <div>
-                      <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg" alt=""></img>
+                      <img className="h-auto max-w-full rounded-lg" src="/homepage/memories/img6.jpg" alt=""></img>
                   </div>
               </div>
               <div className="grid gap-4">
                   <div>
-                      <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg" alt=""></img>
+                      <img className="h-auto max-w-full rounded-lg" src="/homepage/memories/img3.jpg" alt=""></img>
                   </div>
                   <div>
-                      <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg" alt=""></img>
+                      <img className="h-auto max-w-full rounded-lg" src="/homepage/memories/img8.jpg" alt=""></img>
                   </div>
                   <div>
-                      <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg" alt=""></img>
+                      <img className="h-auto max-w-full rounded-lg" src="/homepage/memories/img11.jpg" alt=""></img>
+                  </div>
+              </div>
+              <div className="grid gap-4">
+                  <div>
+                      <img className="h-auto max-w-full rounded-lg" src="/homepage/memories/img10.jpeg" alt=""></img>
+                  </div>
+                  <div>
+                      <img className="h-auto max-w-full rounded-lg" src="/homepage/memories/img7.jpg" alt=""></img>
+                  </div>
+                  <div>
+                      <img className="h-auto max-w-full rounded-lg" src="/homepage/memories/img12.jpg" alt=""></img>
                   </div>
               </div>
           </div>
