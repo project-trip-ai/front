ad37b5486d6ed0304c5574a6d7e5b9c521af801b
Rymuu <sutrils-nozu@outlook.fr>
2024-09-19 11:18:41 +0200

    correction d'un import

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
-import { Inter } from 'next/font/google';
 import './globals.css';
+import { Inter } from 'next/font/google';
 import Header from '@/components/Partials/Header';
 import Footer from '@/components/Partials/Footer';
 import { isUserLoggedIn, getUserCookies } from '@/lib/auth';
